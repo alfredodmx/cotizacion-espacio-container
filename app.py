7aca52d1c31a079bd78e26920038dcbea372b853
alfredodmx <121136339+alfredodmx@users.noreply.github.com>
2026-03-24 20:05:39 -0300

    app.py

--- app.py
+++ app.py
@@ -4462,8 +4462,8 @@ def generar_pdf_completo(carrito_df, subtotal, iva, total, datos_cliente,
     _p50 = formato_clp(round(total * 0.50))
     _p25a = formato_clp(round(total * 0.25))
     _p25b = formato_clp(round(total * 0.25))
-    notas_texto = f"""<b>NOTAS IMPORTANTES:</b><br/>1.- Valores incluyen IVA.<br/>{texto_transporte}<br/>3.- Formas de pago: transferencia - pago contado.<br/>4.- Proceso de pagos:<br/>&nbsp;&nbsp;&nbsp;- 50% inicial correspondiente a <b>{_p50}</b><br/>&nbsp;&nbsp;&nbsp;- 25% obra correspondiente a <b>{_p25a}</b><br/>&nbsp;&nbsp;&nbsp;- 25% entrega correspondiente a <b>{_p25b}</b>."""
-    datos_transferencia = """<b>DATOS PARA TRANSFERENCIA:</b><br/>Inversiones Container House Spa<br/>RUT: 78.268.851-0<br/>Tipo de cuenta: Cuenta Corriente<br/>Banco: Itaú<br/>N° de cuenta: 230771767<br/>Correo: jperez@espaciocontainerhouse.cl"""
+    notas_texto = f"""<font color='#0d2266'><b>NOTAS IMPORTANTES:</b></font><br/>1.- Valores incluyen IVA.<br/>{texto_transporte}<br/>3.- Formas de pago: transferencia - pago contado.<br/>4.- Proceso de pagos:<br/>&nbsp;&nbsp;&nbsp;- 50% inicial correspondiente a <b>{_p50}</b><br/>&nbsp;&nbsp;&nbsp;- 25% obra correspondiente a <b>{_p25a}</b><br/>&nbsp;&nbsp;&nbsp;- 25% entrega correspondiente a <b>{_p25b}</b>."""
+    datos_transferencia = """<font color='#0d2266'><b>DATOS PARA TRANSFERENCIA:</b></font><br/>Inversiones Container House Spa<br/>RUT: 78.268.851-0<br/>Tipo de cuenta: Cuenta Corriente<br/>Banco: Itaú<br/>N° de cuenta: 230771767<br/>Correo: jperez@espaciocontainerhouse.cl"""
     # bloque_notas ya no se usa — se separa en notas_texto y datos_transferencia
 
     totales_data = [
@@ -4746,8 +4746,8 @@ def generar_pdf_cliente(carrito_df, subtotal, iva, total, datos_cliente,
     _p50 = formato_clp(round(total * 0.50))
     _p25a = formato_clp(round(total * 0.25))
     _p25b = formato_clp(round(total * 0.25))
-    notas_texto = f"""<b>NOTAS IMPORTANTES:</b><br/>1.- Valores incluyen IVA.<br/>{texto_transporte}<br/>3.- Formas de pago: transferencia - pago contado.<br/>4.- Proceso de pagos:<br/>&nbsp;&nbsp;&nbsp;- 50% inicial correspondiente a <b>{_p50}</b><br/>&nbsp;&nbsp;&nbsp;- 25% obra correspondiente a <b>{_p25a}</b><br/>&nbsp;&nbsp;&nbsp;- 25% entrega correspondiente a <b>{_p25b}</b>."""
-    datos_transferencia = """<b>DATOS PARA TRANSFERENCIA:</b><br/>Inversiones Container House Spa<br/>RUT: 78.268.851-0<br/>Tipo de cuenta: Cuenta Corriente<br/>Banco: Itaú<br/>N° de cuenta: 230771767<br/>Correo: jperez@espaciocontainerhouse.cl"""
+    notas_texto = f"""<font color='#0d2266'><b>NOTAS IMPORTANTES:</b></font><br/>1.- Valores incluyen IVA.<br/>{texto_transporte}<br/>3.- Formas de pago: transferencia - pago contado.<br/>4.- Proceso de pagos:<br/>&nbsp;&nbsp;&nbsp;- 50% inicial correspondiente a <b>{_p50}</b><br/>&nbsp;&nbsp;&nbsp;- 25% obra correspondiente a <b>{_p25a}</b><br/>&nbsp;&nbsp;&nbsp;- 25% entrega correspondiente a <b>{_p25b}</b>."""
+    datos_transferencia = """<font color='#0d2266'><b>DATOS PARA TRANSFERENCIA:</b></font><br/>Inversiones Container House Spa<br/>RUT: 78.268.851-0<br/>Tipo de cuenta: Cuenta Corriente<br/>Banco: Itaú<br/>N° de cuenta: 230771767<br/>Correo: jperez@espaciocontainerhouse.cl"""
     # bloque_notas ya no se usa — se separa en notas_texto y datos_transferencia
 
     totales_data = [
