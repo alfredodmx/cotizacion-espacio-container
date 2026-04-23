ece7b8a845d7fbeac8007c0e50c46ff0d346a91a
alfredodmx <121136339+alfredodmx@users.noreply.github.com>
2026-04-23 18:45:20 -0400

    app.py

--- app.py
+++ app.py
@@ -807,6 +807,7 @@ if _modo_cliente:
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap');
     #MainMenu,footer,[data-testid="stToolbar"],[data-testid="stDecoration"]{display:none!important;}
     .stAppHeader,.st-emotion-cache-xi6p3a,[data-testid="stAppHeader"]{display:none!important;height:0!important;}
+    .stMainBlockContainer,.block-container,.st-emotion-cache-1ibsh2c{padding-top:0!important;margin-top:0!important;}
     .stApp{background:#f0f4f8 !important;}
     [data-testid="stAppViewContainer"]{background:#f0f4f8 !important;}
     iframe{border:none !important;background:#f0f4f8 !important;}
